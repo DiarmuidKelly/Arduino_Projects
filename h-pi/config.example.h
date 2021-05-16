ff1a96227fd5e291e885de7e10082f2f470dfae6
Diarmuid <diarmuid100@gmail.com>
2021-05-16 21:31:25 +0200

    config

---
+++ h-pi/config.example.h
@@ -0,0 +1,7 @@
+
+#define ssid_config "wifi_ssid";
+#define password_config "password";
+
+#define mqtt_server_config "192.168.0.2";
+#define mqtt_username_config "username";
+#define mqtt_password_config "password";
